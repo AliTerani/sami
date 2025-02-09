2d79fb16709156d085fbced7122dcd015fae049e
AliTerani <70014963+AliTerani@users.noreply.github.com>
2025-02-09 21:11:04 +0300

    Update fetch_events.php

--- src/php/fetch_events.php
+++ src/php/fetch_events.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "193.203.168.121";
+$servername = "193.203.168.122";
 $username = "u402158123_AdminSami";  // Your DB username
 $password = "Sami@2025";              // Your DB password
 $database = "u402158123_manag_db"; // Your DB name
